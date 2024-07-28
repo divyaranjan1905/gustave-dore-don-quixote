31ce469f5f1eb79250b5ad340ba72e47345edc44
Divya Ranjan <divyaranjanpattanaik1905@gmail.com>
2024-07-28 21:52:22 +0000

    Add images and buttons to TOC page

--- chapters/toc.html.pm
+++ chapters/toc.html.pm
@@ -1,3 +1,15 @@
 #lang pollen
 
 ◊(define-meta title "Table of Contents")
+◊(define-meta template "template-toc.html")
+
+◊main{
+	◊h2{History of the Ingenious Gentleman Don Quixote of ◊br{} La Mancha}
+	◊div[#:class "volume-container"]{
+		     ◊div[#:class "cover" #:style "background-image: url('../img/p003.jpg');"]{
+		     		  	  	  ◊button[#:type "button" #:class "volumesbtn volumesbtn-left"]{Volume I}}
+		     ◊div[#:class "separator"]{}
+		     ◊div[#:class "cover" #:style "background-image: url('../img/p44c.jpg');"]{
+		     		     		  ◊button[#:type "button" #:class "volumesbtn volumesbtn-right"]{Volume II}}
+	}
+}
