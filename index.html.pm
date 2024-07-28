a21b565b1dd3c3ae847f343f0b8f947b64a3da94
Divya Ranjan <divyaranjanpattanaik1905@gmail.com>
2024-07-28 08:10:08 +0000

    Removing orig/

--- index.html.pm
+++ index.html.pm
@@ -8,4 +8,4 @@
 		       ◊div[#:class "cover-overlay"]{
 							◊p{◊i{The Ingenious Gentleman,}}
 							◊h1{◊span[#:class "initials"]{D}ON ◊span[#:class "initials"]{Q}UIXOTE}
-		       		    			◊span[#:class "subtitle"]{◊i{◊b{Of}} ◊br{} ◊i{◊b{La Mancha}}}}}}
+		       		    			◊span[#:class "subtitle"]{◊i{Of} ◊i{La Mancha}}}}}
