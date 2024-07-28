c5bbbb433eb06768a4da96455e182b1a26e7dc72
Divya Ranjan <divyaranjanpattanaik1905@gmail.com>
2024-07-28 18:57:54 +0000

    Added a new section to frontpage on dore’s original piece.

--- index.html.pm
+++ index.html.pm
@@ -2,12 +2,26 @@
 
 ◊(define-meta title "Don Quixote")
 ◊(define-meta author "Miguel de Cervantes Saavedra")
+◊(define-meta template "template-index.html")
 
-◊main{◊div[#:class "cover"]{
+◊main{
+	◊div[#:class "cover"]{
 		       ◊div[#:class "cover-bg"]{}
 		       ◊div[#:class "cover-overlay"]{
-							◊p{◊i{The Ingenious Gentleman,}}
-							◊h1{◊span[#:class "initials"]{D}ON ◊span[#:class "initials"]{Q}UIXOTE}
-		       		    			◊span[#:class "subtitle"]{◊i{Of} ◊i{La Mancha}}}
+					◊p{◊i{The Ingenious Gentleman,}}
+					◊h1{◊span[#:class "initials"]{D}ON ◊span[#:class "initials"]{Q}UIXOTE}
+		       		    	◊span[#:class "subtitle"]{◊i{Of} ◊i{La Mancha}}}
 
-							◊button[#:type "button" #:class "read-btn"]{Read}}}
+					◊button[#:type "button" #:class "read-btn"]{Read}}
+
+	◊div[#:class "gustave-dore"]{
+			 ◊div[#:class "cover"]{
+			 	      ◊img[#:src "img/cover.jpg" #:class "img"]{}}
+
+			 ◊h2{Reviving ◊i{Don Quixote}}
+
+			 ◊p{Cervantes' classic tale has enchanted everyone for over centuries, but arguably among the most enchanted was Gustave Doré. Thus, the most celebrated illustrator of 19th century provided us with the most ◊i{definitive} visuals for ◊i{Don Quixote}. This revisualized edition of Doré attempts to revive those marvellous illustrations and provide the book with the typography that it deserves.}
+
+	}
+
+}
