acfa9f3f444fb29a6d427bbf3d10de1be7b22069
Divya Ranjan <divyaranjanpattanaik1905@gmail.com>
2024-07-29 08:33:05 +0000

    Change volume buttons from manual position to flexbox.

--- chapters/toc.html.pm
+++ chapters/toc.html.pm
@@ -7,9 +7,9 @@
 	◊h2{History of the Ingenious Gentleman Don Quixote of ◊br{} La Mancha}
 	◊div[#:class "volume-container"]{
 		     ◊div[#:class "cover" #:style "background-image: url('../img/p003.jpg');"]{
-		     		  	  	  ◊button[#:type "button" #:class "volumesbtn volumesbtn-left"]{Volume I}}
+		     		  	  	  ◊button[#:type "button" #:class "volumesbtn"]{Volume I}}
 		     ◊div[#:class "separator"]{}
 		     ◊div[#:class "cover" #:style "background-image: url('../img/p44c.jpg');"]{
-		     		     		  ◊button[#:type "button" #:class "volumesbtn volumesbtn-right"]{Volume II}}
+		     		     		  ◊button[#:type "button" #:class "volumesbtn"]{Volume II}}
 	}
 }
